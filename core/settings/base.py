2ea30d092e316e28d69c4b6f01299c0b0d8b8a36
Hossam Hamdy <hhamdy@abuerdan.com>
2024-07-23 07:54:07 +0300

    django channels conf

--- core/settings/base.py
+++ core/settings/base.py
@@ -176,3 +176,18 @@ CORS_ALLOW_HEADERS = [
     "currency",
     "display",
 ]
+
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [("127.0.0.1", 6379)],
+#         },
+#     },
+# }
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
