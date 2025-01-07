36014e13ea4fe24897d16661888abcd95b9a0414
Hossam Hamdy <hhamdy@abuerdan.com>
2025-01-07 22:51:04 +0200

    added server name for deploy testing

--- core/settings/production.py
+++ core/settings/production.py
@@ -2,7 +2,7 @@
 
 
 DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "192.168.1.10", "net-rosana-hosamhamdy-798965e4.koyeb.app/"]
+ALLOWED_HOSTS = ["127.0.0.1", "192.168.1.10", "net-rosana-hosamhamdy-798965e4.koyeb.app"]
 # CORS_ALLOW_ALL_ORIGINS = True
 # CSRF_COOKIE_SECURE = True
 # SESSION_COOKIE_SECURE = True
@@ -11,4 +11,4 @@ ALLOWED_HOSTS = ["127.0.0.1", "192.168.1.10", "net-rosana-hosamhamdy-798965e4.ko
 # SECURE_SSL_REDIRECT = True
 # SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
-CSRF_TRUSTED_ORIGINS = ["http://192.168.1.10:9000", "http://127.0.0.1:9000", "https://net-rosana-hosamhamdy-798965e4.koyeb.app/"]
+CSRF_TRUSTED_ORIGINS = ["http://192.168.1.10:9000", "http://127.0.0.1:9000", "https://net-rosana-hosamhamdy-798965e4.koyeb.app"]
