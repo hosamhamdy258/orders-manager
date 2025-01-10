104b7afdd0ff3669d8de0716b82305c8ed700883
Hossam Hamdy <hhamdy@abuerdan.com>
2025-01-10 09:53:16 +0200

    fix .env file creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-
+# ===========================================
 # Run the log directory creation script
 python initialize_logs.py
+# ===========================================
+
+
+
+# ===========================================
+# Static Files Generation in Production
+# ===========================================
 
 # echo "DJANGO_DEVELOPMENT is set to: $DJANGO_DEVELOPMENT"
 
@@ -15,9 +22,37 @@ if [ "$DJANGO_DEVELOPMENT" = "False" ] && [ ! -f "$MARKER_FILE" ]; then
   # Create the marker file to indicate collectstatic has been run
   mkdir -p "$(dirname "$MARKER_FILE")"
   touch "$MARKER_FILE"
-else
-  echo "Skipping collectstatic (either already run or DJANGO_DEVELOPMENT is True)."
+# else
+#   echo "Skipping collectstatic (either already run or DJANGO_DEVELOPMENT is True)."
 fi
+# ===========================================
+
+
+
+# ===========================================
+# Environment Section
+# ===========================================
+
+# Path to the .env file
+ENV_FILE="./core/settings/.env"
+
+# Check if .env file exists
+if [ ! -f "$ENV_FILE" ]; then
+  echo ".env file not found in /core/settings. Creating one with a generated SECRET_KEY."
+  
+  # Generate a SECRET_KEY using Python and write it to the .env file
+  SECRET_KEY=$(python -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())")
+  
+  # Create the .env file and add the SECRET_KEY
+  echo "DJANGO_SECRET_KEY=$SECRET_KEY" > "$ENV_FILE"
+  echo "DJANGO_DEVELOPMENT=False" >> "$ENV_FILE"
+
+# else
+#   echo ".env file found in /core/settings. Skipping creation."
+fi
+# ===========================================
+
+
 
 # Execute the main command (passed as CMD in Dockerfile)
 exec "$@"
