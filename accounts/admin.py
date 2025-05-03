cadb08aa7724cdbf895c97d543d62211c789b6cf
Hossam Hamdy <hhamdy@abuerdan.com>
2025-05-03 20:32:40 +0300

    added global site configurations with singleton

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from solo.admin import SingletonModelAdmin
 
-from .models import User
+from .models import Configuration, User
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Configuration, SingletonModelAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from solo.models import SingletonModel
 
 
 class User(AbstractUser):
     email = models.EmailField(verbose_name=_("email address"), unique=True)
+
+
+class Configuration(SingletonModel):
+    site_name = models.CharField(max_length=255, default="Site Name")
+    maintenance_mode = models.BooleanField(default=False)
+    order_limit = models.IntegerField(_("Order Limit"), default=1)
+    order_archive_delay = models.IntegerField(_("Order Archive Delay Hours"), default=1)
+    order_time_limit = models.IntegerField(_("Order Time Limit"), default=15)
+
+    def __str__(self):
+        return "Site Configuration"
+
+    class Meta:
+        verbose_name = "Site Configuration"
+
+
+def configuration():
+    return Configuration.get_solo()
